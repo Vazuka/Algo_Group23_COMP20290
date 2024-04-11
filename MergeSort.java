b7097b36b25ed9ec56c53de4744bc344b412c315
NikkiBoss-8 <102314680+NikkiBoss-8@users.noreply.github.com>
2024-04-11 11:16:52 +0100

    Created testing MergeSort Java File

---
+++ MergeSort.java
@@ -0,0 +1,61 @@
+import java.util.Arrays;
+import java.util.Comparator;
+
+public class MergeSort {
+
+    public static <T extends Comparable<? super T>> void mergesort(final T[] array, final Comparator<T> comparator) {
+        int length = array.length;
+        if (length < 2) {
+            return;
+        }
+
+        final T[] left = Arrays.copyOfRange(array, 0, array.length / 2);
+        final T[] right = Arrays.copyOfRange(array, array.length / 2, array.length);
+
+        mergesort(left, comparator);
+        mergesort(right, comparator);
+
+        int leftIndex = 0;
+        int rightIndex = 0;
+        int index = 0;
+
+        while (leftIndex < left.length && rightIndex < right.length) {
+            T leftItem = left[leftIndex];
+            T rightItem = right[rightIndex];
+
+            // If left < right
+            if (comparator.compare(leftItem, rightItem) < 0) {
+                array[index] = left[leftIndex];
+                leftIndex += 1;
+            } else {
+                array[index] = right[rightIndex];
+                rightIndex += 1;
+            }
+            index += 1;
+        }
+
+        // merge the remaining elements(if there are any)
+        while (leftIndex < left.length) {
+            array[index] = left[leftIndex];
+            leftIndex += 1;
+            index += 1;
+        }
+
+        while (rightIndex < right.length) {
+            array[index] = right[rightIndex];
+            rightIndex += 1;
+            index += 1;
+        }
+    }
+
+    public static void main(String[] args) {
+        // Example usage
+        Integer[] array = {5, 2, 9, 1, 3, 7, 4, 8, 6};
+
+        System.out.println("Original Array: " + Arrays.toString(array));
+
+        mergesort(array, Comparator.naturalOrder());
+
+        System.out.println("Merge Sorted Array: " + Arrays.toString(array));
+    }
+}
