540194ed31a414c829f4385c9877e4e2b8ec3509
NikkiBoss-8 <102314680+NikkiBoss-8@users.noreply.github.com>
2024-04-23 20:02:48 +0100

    Update MSQSA.py

--- code/MSQSA.py
+++ code/MSQSA.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 x_values = ['1','3','5','9']
 y1_values = [0.3649, 0.2983, 0.2424, 0.207]
 y2_values = [1.1433, 0.9718, 0.9302, 0.8826]
-y3_values = [5.5161, 5.2466, 4.3526, 0.38743]
+y3_values = [5.5161, 5.2466, 4.3526, 2.38743]
 
 # Create the line graph
 plt.plot(x_values, y1_values, label='1 million', marker='o')
